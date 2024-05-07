e64d1eff00b03f5a5906d218cbf23300b8b69ae9
janahani <125489372+janahani@users.noreply.github.com>
2024-05-07 18:15:20 +0300

    Add files via upload

---
+++ Gym Web Application/Gym Web Application/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Gym_Web_Application.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
