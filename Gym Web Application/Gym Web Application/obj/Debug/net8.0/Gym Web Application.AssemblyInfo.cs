602480a3c2399caa63520d65c6f4c41e2b7e6525
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2024-05-11 02:21:15 +0300

    Build changes.

--- Gym Web Application/Gym Web Application/obj/Debug/net8.0/Gym Web Application.AssemblyInfo.cs
+++ Gym Web Application/Gym Web Application/obj/Debug/net8.0/Gym Web Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f13f78b834d91d585c2f55058cf23746616d25f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33765932f6c1d0d20e3a56112e07e13fb965ce78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
