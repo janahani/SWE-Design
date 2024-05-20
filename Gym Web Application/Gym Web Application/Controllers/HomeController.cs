b90c1cfc762e250f9420c9415b9a8aea8198f381
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2024-05-20 21:38:00 +0300

    Updated home controller for login.

--- Gym Web Application/Gym Web Application/Controllers/HomeController.cs
+++ Gym Web Application/Gym Web Application/Controllers/HomeController.cs
@@ -4,30 +4,57 @@ using Gym_Web_Application.Models;
 
 namespace Gym_Web_Application.Controllers;
 
-public class HomeController : Controller
-{
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly EmployeeService _employeeService;
 
-    [HttpGet]
-    public IActionResult Dashboard()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, EmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
 
-    [HttpGet]
-    public IActionResult Login()
-    {
-        return View();
-    }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            EmployeeModel employee = new EmployeeModel();
+            return View(employee);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpPost]
+        public async Task<IActionResult> LoginEmployee(EmployeeModel model)
+        {
+            var employee = await _employeeService.GetEmployeeByEmail(model.Email);
+            if(employee != null)
+            {
+                if (await _employeeService.ValidateEmployeeLogin(model.Email, model.Password))
+                {
+                    return RedirectToAction("Dashboard");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Invalid password.";
+                    return View("Login");
+                }
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Email not found.";
+                return View("Login");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-}
