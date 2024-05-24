e1ab85ca3b486ce3a4b039ca4a2299a457ae146d
janahani <Jana2100237@miuegypt.edu.eg>
2024-05-24 23:34:51 +0200

    adjust get of dashboard

--- Gym Web Application/Gym Web Application/Controllers/HomeController.cs
+++ Gym Web Application/Gym Web Application/Controllers/HomeController.cs
@@ -5,71 +5,80 @@ using Gym_Web_Application.Models;
 namespace Gym_Web_Application.Controllers;
 
 
-    public class HomeController : Controller
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly EmployeeService _employeeService;
+    private readonly DashboardService _dashboardService;
+
+    public HomeController(ILogger<HomeController> logger, EmployeeService employeeService, DashboardService dashboardService)
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly EmployeeService _employeeService;
+        _logger = logger;
+        _employeeService = employeeService;
+        _dashboardService = dashboardService;
+    }
 
-        public HomeController(ILogger<HomeController> logger, EmployeeService employeeService)
-        {
-            _logger = logger;
-            _employeeService = employeeService;
-        }
+    [HttpGet]
+    public IActionResult Login()
+    {
+        EmployeeModel employee = new EmployeeModel();
+        return View(employee);
+    }
 
-        [HttpGet]
-        public IActionResult Login()
+    [HttpPost]
+    public async Task<IActionResult> LoginEmployee(EmployeeModel model)
+    {
+        var employee = await _employeeService.GetEmployeeByEmail(model.Email);
+        if (employee != null)
         {
-            EmployeeModel employee = new EmployeeModel();
-            return View(employee);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> LoginEmployee(EmployeeModel model)
-        {
-            var employee = await _employeeService.GetEmployeeByEmail(model.Email);
-            if(employee != null)
+            if (await _employeeService.ValidateEmployeeLogin(model.Email, model.Password))
             {
-                if (await _employeeService.ValidateEmployeeLogin(model.Email, model.Password))
-                {
-                    string JobTitleID = employee.JobTitleID.ToString();
-                    HttpContext.Session.SetString("EmployeeEmail", employee.Email);
-                    HttpContext.Session.SetString("EmployeeJobTitleID", JobTitleID);
-                    return RedirectToAction("Dashboard");
-                }
-                else
-                {
-                    ViewBag.ErrorMessage = "Invalid password.";
-                    return View("Login");
-                }
+                string JobTitleID = employee.JobTitleID.ToString();
+                HttpContext.Session.SetString("EmployeeEmail", employee.Email);
+                HttpContext.Session.SetString("EmployeeJobTitleID", JobTitleID);
+                return RedirectToAction("Dashboard");
             }
             else
             {
-                ViewBag.ErrorMessage = "Email not found.";
+                ViewBag.ErrorMessage = "Invalid password.";
                 return View("Login");
             }
         }
-
-        [HttpGet]
-        public IActionResult Dashboard()
+        else
         {
-            if (HttpContext.Session.GetString("EmployeeEmail") == null)
-            {
-                return RedirectToAction("Login");
-            }
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Logout()
-        {
-            HttpContext.Session.Remove("EmployeeEmail");
-            HttpContext.Session.Remove("EmployeeJobTitleID");
-            return RedirectToAction("Login");
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewBag.ErrorMessage = "Email not found.";
+            return View("Login");
         }
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Dashboard()
+    {
+        if (HttpContext.Session.GetString("EmployeeEmail") == null)
+        {
+            return RedirectToAction("Login");
+        }
+        ViewBag.Top3RecentClients = await _dashboardService.GetTop3RecentClients();
+        ViewBag.PackageCount = await _dashboardService.GetPackageCount();
+        ViewBag.ClientCount = await _dashboardService.GetClientCount();
+        ViewBag.EmployeeCount = await _dashboardService.GetEmployeeCount();
+        ViewBag.CoachCount = await _dashboardService.GetCoachCount();
+        ViewBag.ClassCount = await _dashboardService.GetClassCount();
+        ViewBag.ActivatedMembershipCount = await _dashboardService.GetActivatedMembershipCount();
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Remove("EmployeeEmail");
+        HttpContext.Session.Remove("EmployeeJobTitleID");
+        return RedirectToAction("Login");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
