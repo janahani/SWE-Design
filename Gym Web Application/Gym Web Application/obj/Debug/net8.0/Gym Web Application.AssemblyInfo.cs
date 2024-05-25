573a412ffe594ba607e65fbe1f64aeca60e0bf24
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2024-05-25 09:49:43 +0300

    correct smtp mail pw

--- Gym Web Application/Gym Web Application/obj/Debug/net8.0/Gym Web Application.AssemblyInfo.cs
+++ Gym Web Application/Gym Web Application/obj/Debug/net8.0/Gym Web Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2bd33ca7de3db5435db3e3d6310c1b862737ff9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687e230c3618a199a8a3035fa4e465c18ccf83a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
