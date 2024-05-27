079de618e12e49b0d960df82344a365cafa2e62d
janahani <Jana2100237@miuegypt.edu.eg>
2024-05-27 21:24:59 +0200

    all employees auth factories

--- Gym Web Application/Gym Web Application/FactoryDP/SalesAuthFactory.cs
+++ Gym Web Application/Gym Web Application/FactoryDP/SalesAuthFactory.cs
@@ -3,13 +3,13 @@ using Gym_Web_Application.Data;
 namespace Gym_Web_Application.FactoryDP;
 using Microsoft.EntityFrameworkCore;
 
-public class AdminAuthFactory : AuthoritiesFactory
+public class SalesAuthFactory : AuthoritiesFactory
 {
    private readonly DbContextOptions<AppDbContext> _options;
    private readonly IWebHostEnvironment _hostingEnvironment;
    private readonly ISalesReportObservable _salesReportObservable;
 
-    public AdminAuthFactory(DbContextOptions<AppDbContext> options,IWebHostEnvironment hostingEnvironment,ISalesReportObservable salesReportObservable)
+    public SalesAuthFactory(DbContextOptions<AppDbContext> options,IWebHostEnvironment hostingEnvironment,ISalesReportObservable salesReportObservable)
     {
         _options = options;
         _hostingEnvironment = hostingEnvironment;
@@ -19,7 +19,7 @@ public class AdminAuthFactory : AuthoritiesFactory
     public List<AuthorityModel> createAuthorities()
     {
         using var dbContext= new AppDbContext(_options);
-        List<AuthorityModel> adminAuth = new List<AuthorityModel>();
+        List<AuthorityModel> salesAuth = new List<AuthorityModel>();
 
         ClientAuthoritiesFactory clientAuthoritiesFactory = new ClientAuthoritiesFactory(_options);
         EmployeeAuthoritiesFactory empAuthoritiesFactory = new EmployeeAuthoritiesFactory(_options);
@@ -29,15 +29,15 @@ public class AdminAuthFactory : AuthoritiesFactory
         DashboardAuthoritiesFactory dashboardAuthoritiesFactory = new DashboardAuthoritiesFactory(_options);
         ReportAuthoritiesFactory reportAuthoritiesFactory = new ReportAuthoritiesFactory(_options,_salesReportObservable);
 
-        adminAuth.Add(clientAuthoritiesFactory);
-        adminAuth.Add(classAuthoritiesFactory);
-        adminAuth.Add(empAuthoritiesFactory);
-        adminAuth.Add(membershipAuthoritiesFactory);
-        adminAuth.Add(packagesAuthoritiesFactory);
-        adminAuth.Add(dashboardAuthoritiesFactory);
-        adminAuth.Add(reportAuthoritiesFactory);
+        salesAuth.Add(clientAuthoritiesFactory);
+        salesAuth.Add(classAuthoritiesFactory);
+        salesAuth.Add(empAuthoritiesFactory);
+        salesAuth.Add(membershipAuthoritiesFactory);
+        salesAuth.Add(packagesAuthoritiesFactory);
+        salesAuth.Add(dashboardAuthoritiesFactory);
+        salesAuth.Add(reportAuthoritiesFactory);
 
-        return adminAuth;
+        return salesAuth;
 
     }
 }
