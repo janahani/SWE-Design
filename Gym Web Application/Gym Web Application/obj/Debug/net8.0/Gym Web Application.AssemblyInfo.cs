daa43886e5c60d5b5a65e463d00619d1ddb532c1
malakHelmy <121347791+malakHelmy@users.noreply.github.com>
2024-05-27 21:39:22 +0300

    push

--- Gym Web Application/Gym Web Application/obj/Debug/net8.0/Gym Web Application.AssemblyInfo.cs
+++ Gym Web Application/Gym Web Application/obj/Debug/net8.0/Gym Web Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96bbc0c45d01659bdc3b67212eda371e9a152ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b2d17bb8270e068f180d38d465eabced0871af1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gym Web Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
